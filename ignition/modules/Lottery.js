f0959a86eac7a533596e9f1d3c125f4754f43255
NecipAkgz <nexco@hotmail.com>
2025-09-21 02:14:35 +0300

    chore: remove unnecessary files

--- ignition/modules/Lottery.js
+++
@@ -1,9 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-const LotteryModule = buildModule("LotteryModule", (m) => {
-  const lottery = m.contract("ConfidentialLottery");
-
-  return { lottery };
-});
-
-module.exports = LotteryModule;
